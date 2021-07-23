a51255d36ae874fea0cfb1b7bebc7313b2c21b78
Joel Santana <joelsantzdev@gmail.com>
2021-07-23 04:10:56 -0500

    add function to call api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
 
-const Home = () => (
-  <div>
-    <h1>COMICS</h1>
-  </div>
-)
+const Home = () => {
+
+  // app state
+  const [ search, setSearch ] = useState('');
+  const [ comics, setComics ] = useState([]);
+
+  useEffect(() => {
+    const getComics = async () => {
+      if(search === '') return;
+
+      const comicsPerPage = 20;
+      const apiKey = '';
+      const url = ``;
+
+      const response = await fetch(url);
+      const result = await response.json();
+
+      console.log(result);
+      setComics('TODO: setComics(result.comics)')
+    }
+    getComics();
+  }, [search])
+
+  return (
+    <div>
+      <h1>COMICS</h1>
+      //TODO: form to setSearch
+    </div>
+  );
+}
 
 export default Home;
